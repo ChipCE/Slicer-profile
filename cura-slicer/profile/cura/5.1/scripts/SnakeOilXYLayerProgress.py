99277b427647701b5c4e5f7c3986fa82b5655285
ChipCE <login721@gmail.com>
2023-01-05 15:42:10 +0900

    update klipper config and post-processing scripts

--- cura-slicer/profile/cura/5.1/scripts/SnakeOilXYLayerProgress.py
+++ cura-slicer/profile/cura/5.1/scripts/SnakeOilXYLayerProgress.py
@@ -3,22 +3,18 @@ import re  # To perform the search and replace.
 from ..Script import Script
 
 
-class KlipperPostProcessing(Script):
+class SnakeOilXYLayerProgress(Script):
 
     def getSettingDataString(self):
         return """{
-            "name": "Klipper Post Processing",
-            "key": "KlipperPostProcessing",
+            "name": "SnakeOilXY leyer progress",
+            "key": "SnakeOilXYLayerProgress",
             "metadata": {},
             "version": 2,
             "settings":{}
         }"""
 
     def execute(self, data):
-
-        minMaxXY = {'MINX': 0, 'MINY': 0, 'MAXX': 0, 'MAXY': 0}
-        startGcodeLineData = ''
-
         totalLayerCount = ''
         currentLayer = ''
         gcodeTotalPrintTime = ''
@@ -26,25 +22,6 @@ class KlipperPostProcessing(Script):
         gcodePercentProgress = ''
 
         for layerNumber, layerData in enumerate(data):
-
-            # search for print area boundary
-            for k, v in minMaxXY.items():
-                result = re.search(str(k)+":(\d*\.?\d*)", layerData)
-                if result is not None:
-                    minMaxXY[k] = result.group(1)
-            # search for set print area macro
-            areaStartGcode = re.search(
-                ".*%(MINX|MAXX|MINY|MAXY)%.*", layerData)
-            # replace print area template
-            if areaStartGcode is not None:
-                if not startGcodeLineData:
-                    startGcodeLineData = layerData
-                for k, v in minMaxXY.items():
-                    pattern3 = re.compile('%' + k + '%')
-                    startGcodeLineData = re.sub(
-                        pattern3, v, startGcodeLineData)
-                data[layerNumber] = startGcodeLineData
-
             # get total layer count
             if not totalLayerCount:
                 result = re.search(r";LAYER_COUNT:(\d*)", layerData)
@@ -55,7 +32,6 @@ class KlipperPostProcessing(Script):
                 result = re.search(r";LAYER:(\d*)", layerData)
                 if result is not None:
                     currentLayer = result.group(1)
-
             # get total print time
             if not gcodeTotalPrintTime:
                 result = re.search(r";TIME:(\d*)", layerData)
@@ -73,14 +49,10 @@ class KlipperPostProcessing(Script):
 
             # insert progress code
             if currentLayer and gcodeRemainPrintTime and gcodePercentProgress:
-                data[layerNumber] = "DISPLAY_GCODE_PROGRESS TOTAL_LAYER=" + str(totalLayerCount) + " CURRENT_LAYER=" + str(
+                data[layerNumber] = "GCODE_PROGRESS TOTAL_LAYER=" + str(totalLayerCount) + " CURRENT_LAYER=" + str(
                     currentLayer) + " PROGRESS=" + str(gcodePercentProgress) + " REMAIN=" + str(gcodeRemainPrintTime) + "\n" + layerData
 
         return data
 
-
-# start g-code format
-# START_PRINT EXTRUDER_TEMP={material_print_temperature_layer_0} BED_TEMP={material_bed_temperature_layer_0} AREA_START=%MINX%,%MINY% AREA_END=%MAXX%,%MAXY%
-
 # layer progress format
-# SET_GCODE_PROGRESS TOTAL_LAYER=37 CURRENT_LAYER=0 PROGRESS=4 REMAIN=0:5
+# GCODE_PROGRESS TOTAL_LAYER=37 CURRENT_LAYER=0 PROGRESS=4 REMAIN=0:5
